8442739fdae6d5b9763752714b2244f71e3e4c4b
康永胜 <kys8823@gmail.com>
2017-01-11 08:26:35 +0800

    test-02.pl

---
+++ mastering-regular-expressions/chap-4/test-02.pl
@@ -0,0 +1,12 @@
+$url = "http://localhost:3000/user?dialog=true";
+
+if($url =~ m{^http://([^/:]*)(?::(\d+))?(/.*)?}i){
+  my $host = $1;
+  my $port = $2;
+  my $path = $3;
+  print "Host: $host\n";
+  print "Port: $port\n";
+  print "Path: $path\n";
+} else {
+  print "Not a http url.";
+}
