a7a9a5e1c952f5d8ee4d8a38372883f23976faed
carose-virtualbox <kys8823@gmail.com>
2017-05-26 13:35:30 +0800

    init poker.c

---
+++ C-programming-A-modern-approach-2th/chap-10/poker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+const char rank[13] = {'2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'q', 'k', 'a'};
+const char 
+
+void read_cards(void);
+void analyze_hand(void);
+void print_result(void);
+
+int main(){
+
+  return 0;
+}
+
+void read_cards(void){
+  
+}
+
+void analyze_hand(void){
+
+}
+
+void print_result(void){
+
+}
