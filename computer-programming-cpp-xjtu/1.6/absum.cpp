88b030d54e4339fd7e4663f4cd61c67277e2a92a
康永胜 <kys8823@gmail.com>
2017-10-26 15:09:29 +0800

    absum.cpp

---
+++ computer-programming-cpp-xjtu/1.6/absum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+	int a, b;
+	
+	cout << "Please enter tow numbers" << endl;
+	
+	cin >> a;
+	cin >> b;
+	
+	cout << a << "+" << b << "=" << a + b << endl;
+}
