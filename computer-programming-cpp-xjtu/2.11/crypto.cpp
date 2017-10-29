ad2c17adf131af9377f993c17c6239e23cb40493
康永胜 <kys8823@gmail.com>
2017-10-29 16:13:13 +0800

    add new example

---
+++ computer-programming-cpp-xjtu/2.11/crypto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (void){
+	char c;
+	
+	cout << "Please enter a char: ";
+	cin  >> c;
+	
+	cout << "Your enter is: " << c << endl;
+	
+	cout << "The ciphertext is " << (char)((c - 'a' + 3) % 26 + 'a') << endl;
+	
+}
